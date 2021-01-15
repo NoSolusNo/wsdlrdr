db74fe21ab8a5910f36169e0662fb6f20eb3c2db
NoSolusNo <51202207+NoSolusNo@users.noreply.github.com>
2021-01-15 12:26:54 +0200

    Create usercode.js

---
+++ usercode.js
@@ -0,0 +1,14 @@
+const Wsdlrdr = require('wsdlrdr');
+const params = {
+    host: 'api-tt.belavia.by',
+    wsdl: '/TimeTable/Service.asmx?WSDL'
+};
+
+const options = {
+    secure: true,
+    failOnWrongContentType: true
+};
+
+module.exports = (data) => {
+    data.test = Wsdlrdr.getNamespaces();
+};
