9cc50f94858c3f7c415cd242c6527506d60934a1
NoSolusNo <51202207+NoSolusNo@users.noreply.github.com>
2021-01-15 12:39:35 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,4 +1,4 @@
-require("wsdlrdr");
+const Wsdlrdr = require("wsdlrdr");
 const params = {
     host: 'api-tt.belavia.by',
     wsdl: '/TimeTable/Service.asmx?WSDL'
@@ -10,5 +10,5 @@ const options = {
 };
 
 module.exports = (data) => {
-    data.test = wsdlrdr.getNamespaces();
+    data.test = Wsdlrdr.getNamespaces(params, options);
 };
